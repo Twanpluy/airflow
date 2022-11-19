5eb088b9e59d69f9c08e1b1c2755ef57b8f1fdd1
Twan Pluymaekers <twanpluy@gmail.com>
2022-11-19 12:48:53 +0100

    Firsst step creating ebay call with airflow

---
+++ dags/search_ebay.py
@@ -0,0 +1,11 @@
+"""
+This DAG will search ebay for a given keyword and save the results to a json file.
+
+TODO: Add DAG to make api requests to ebay and save the results to a json file.
+STEP 1: Create a DAG that will check if API is up.
+STEP 2: Create a search request to ebay.
+STEP 3: Save the results to a json file.
+"""
+
+
+
